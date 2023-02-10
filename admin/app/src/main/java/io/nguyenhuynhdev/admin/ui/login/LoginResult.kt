7452bbe09ce598ffab290c1976116cb088e22884
NguyenHuynh <nguyenhuynhdev@gmail.com>
2023-02-10 19:12:03 +0700

    Init

---
+++ admin/app/src/main/java/io/nguyenhuynhdev/admin/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package io.nguyenhuynhdev.admin.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
